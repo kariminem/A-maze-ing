4d66cb13b0ab2a3e3a4d65aab1d1e5e6208c8d26
kariminem <karimwael96@gmail.com>
2026-06-19 11:55:24 +0200

    started config, input parsing

---
+++ config.py
@@ -0,0 +1,10 @@
+
+
+## here all I want to do is to read a file
+## parse its **input** --> checking for errors
+## split each line from the '='
+## create a dictioanry to hold the input
+## numbers are numbers, strings are strings
+## handle the point conversion of (0,0)
+## when all is done and clear for takeoff, we 
+## return the dict for the next phase!
